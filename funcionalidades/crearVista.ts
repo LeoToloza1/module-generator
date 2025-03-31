dae90629585ca4471515fb36860e23836bc263d7
Toloza  Leonel <leotoloza6@gmail.com>
2025-03-31 18:21:37 -0300

    actualizar la lógica de creación de vistas para manejar rutas de archivos base; actualizar versión a 1.0.5 en package.json

--- funcionalidades/crearVista.ts
+++ funcionalidades/crearVista.ts
@@ -1,60 +1,64 @@
 import * as fs from "node:fs/promises";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { styleText } from "node:util";
 
 export default class CrearVista {
-    private _nombreModulo: string = "";
+    private _nombreModulo: string;
     private baseDir: string = process.cwd();
-    private directorioCreado: string = "";
+    private directorioCreado: string;
 
     constructor(nombreModulo: string) {
         this._nombreModulo = nombreModulo;
-    }
-
-    public get nombreModulo(): string {
-        return this._nombreModulo;
+        this.directorioCreado = path.join(this.baseDir, "modulos", this._nombreModulo, "views");
     }
 
     public set nombreModulo(nombre: string) {
         this._nombreModulo = nombre;
+        this.directorioCreado = path.join(this.baseDir, "modulos", this._nombreModulo, "views"); // Se actualiza la ruta
     }
 
     public async crearVista() {
-
-        const carpetaModulos = path.join(this.baseDir, "modulos");
-        this.directorioCreado = path.join(carpetaModulos, this._nombreModulo, "views");
-
         try {
-
             await fs.mkdir(this.directorioCreado, { recursive: true });
             console.log("\n");
             console.log(styleText(["gray", "bgGreenBright"], `Directorio de vistas creado en: ${this.directorioCreado}`));
             console.log("\n");
-            const archivos: string[] = await fs.readdir(path.join(this.baseDir, "vistas_base"));
-            await this.crearArchivos(archivos);
+
+            let archivosBasePath = path.join(this.baseDir, "vistas_base");
+            try {
+                await fs.access(archivosBasePath);
+            } catch {
+                const packagePath = path.dirname(fileURLToPath(import.meta.url));
+                archivosBasePath = path.join(packagePath, "../../vistas_base");
+            }
+
+            console.log(styleText(["blueBright"], `Usando archivos base desde: ${archivosBasePath}`));
+
+            const archivos: string[] = await fs.readdir(archivosBasePath);
+            await this.crearArchivos(archivos, archivosBasePath);
 
         } catch (error) {
-            console.error(`Error al crear el módulo: ${error}`);
+            console.error(styleText(["redBright"], `Error al crear la vista: ${error}`));
         }
     }
 
-    private async crearArchivos(archivos: string[]): Promise<void> {
+    private async crearArchivos(archivos: string[], archivosBasePath: string): Promise<void> {
         console.log(styleText(["gray", "bgGreenBright"], `Creando archivos en: ${this.directorioCreado}`));
-        for (const archivoBase of archivos) {
-            const archivoBasePath = path.join(this.baseDir, "vistas_base", archivoBase);
-            try {
 
+        for (const archivoBase of archivos) {
+            const archivoBasePath = path.join(archivosBasePath, archivoBase);
+            try {
                 let contenido = await fs.readFile(archivoBasePath, "utf-8");
                 //la letra g es para que busque en todo el texto y reemplace en todos los lugares deonde se usa
                 contenido = contenido.replace(/{{nombreModulo}}/g, this._nombreModulo);
 
                 const nuevoArchivoPath = path.join(this.directorioCreado, archivoBase);
-
                 await fs.writeFile(nuevoArchivoPath, contenido);
 
-                console.log(styleText(["gray", "bgGreenBright"], `Archivo creado: ${nuevoArchivoPath}`));
+                console.log(styleText(["greenBright"], `Archivo creado: ${nuevoArchivoPath}`));
             } catch (error) {
-                console.error(`Error al leer o escribir el archivo ${archivoBase}: ${error}`);
+                console.error(styleText(["redBright"], `Error al leer o escribir el archivo ${archivoBase}: ${error}`));
             }
         }
     }
